c8071fd5cc5b076c82841623e51c7963ada709db
dk188 <dk188@LAPTOP-UVIC0HB6>
2022-09-23 02:10:30 -0600

    Agregar archivos de proyecto.

---
+++ DIVICION_PROPIEDADES/DIVICION.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIVICION_PROPIEDADES
+{
+    internal class DIVICION
+    {
+        internal class cldivisión
+        {
+            // Propiedades, campos, etc.
+            private double n1;
+            private double n2;
+
+            // Encapsulamiento de las propiedades.
+            public double N1 { get => n1; set => n1 = value; }
+            public double N2 { get => n2; set => n2 = value; }
+
+            // Metodos.
+            public double Dividir()
+            {
+                return n1 / n2;
+            }
+        }
+    }
+}
+
---
+++ DIVICION_PROPIEDADES/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static DIVICION_PROPIEDADES.DIVICION;
+
+namespace DIVICION_PROPIEDADES
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        
+            {
+                // Instancia de la clase cldivisión.
+                cldivisión odivision = new cldivisión();
+                // Se le asigna a la propiedad N1 el valor dado en la textBox1.
+                odivision.N1 = double.Parse(textBox1.Text);
+                // Se le asigna a la propiedad N2 el valor dado en la textBox2.
+                odivision.N2 = double.Parse(textBox2.Text);
+                // Se le asigna a la textBox3 el resultado que se le aplica al objeto odivision con el metodo Dividir y se pasa a tipo string.
+                textBox3.Text = odivision.Dividir().ToString();
+            }
+        }
+    }
